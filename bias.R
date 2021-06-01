a35b8f9819abea43aec3b0d463af41f9240188c0
Elizabeth Morris <elizabeth.morris@colostate.edu>
2021-06-01 14:10:36 -0600

    cleaned

--- bias.R
+++ bias.R
@@ -4,18 +4,13 @@ library(MASS) # for 2D kernel density function
 library(magrittr) # for piping functionality, i.e., %>%
 library(maptools) # reading shapefiles
 library(tidyverse)
-<<<<<<< HEAD
 library(usdm)
-=======
-
->>>>>>> 140eae60836dd87ba15aedebf223b2f14a57dd47
 ##################################################
 ### to begin: run all of data, VIF,           ###
 ### and for_maxent scripts first              ###         
 ################################################
 
-<<<<<<< HEAD
-#A combination of every single Aedes we have 
+#Using all Aedes data from all data sources
 
 globe <- globe %>%
   rename(OCCURRENCE_ID = site_id) %>%
@@ -34,16 +29,6 @@ all_aedes <- full_join(two, lit_99_08, by = c("X", "Y", "OCCURRENCE_ID"))
 climdat <- brick(ma.bio) #set climate data
 
 locations <- all_aedes %>% #set presence location data
-=======
-#Old lit bias file 
-
-old_lit <- lit_99_08 %>% #clean down to Aedes aegypti
-  filter(VECTOR == "Aedes aegypti")
-  
-climdat <- brick(ma.bio) #set climate data
-
-locations <- old_lit %>% #set presence location data
->>>>>>> 140eae60836dd87ba15aedebf223b2f14a57dd47
   dplyr::select(X, Y)
 
 occur.ras <- rasterize(locations, climdat, 1)
@@ -65,72 +50,5 @@ dens.work <- resample(dens.ras, ma.bio) #also resize raster
 
 plot(dens.work)
 
-<<<<<<< HEAD
 writeRaster(dens.work, filename = "bias_all_aedes.asc", format = "ascii", overwrite = TRUE)
-=======
-writeRaster(dens.work, filename = "bias_oldlit.asc", format = "ascii", overwrite = TRUE)
-
-#GLOBE bias file 
-
-globe_aeg <- globe %>%
-  filter(`mosquito habitat mapper:species` == "aegypti")
-
-climdat <- brick(ma.bio)
-
-locations1 <- globe_aeg %>%
-  dplyr::select(longitude, latitude)
-
-occur.ras1 <- rasterize(locations1, climdat, 1)
-
-plot(occur.ras1)
-
-presences1 <- which(values(occur.ras1) == 1)
-pres.locs1 <- coordinates(occur.ras1)[presences1, ]
-
-dens1 <- kde2d(pres.locs1[,1], pres.locs1[,2], n = c(nrow(occur.ras1), ncol(occur.ras1)))
-dens.ras1 <- raster(dens1)
-plot(dens.ras1)
-
-dens.ras1 <- extend(dens.ras1, ma.area)
-
-plot(dens.ras1)
-
-dens.work1 <- resample(dens.ras1, ma.bio)
-
-plot(dens.work1)
-
-writeRaster(dens.work1, filename = "bias_globe.asc", format = "ascii", overwrite = TRUE)
-
-#New lit bias file 
-
-new_lit <- lit_09_18 %>%
-  filter(VECTOR == "Aedes aegypti")
-
-climdat <- brick(ma.bio)
-
-locations2 <- new_lit %>%
-  dplyr::select(X, Y)
-
-occur.ras2 <- rasterize(locations2, climdat, 1)
-
-plot(occur.ras2)
-
-presences2 <- which(values(occur.ras2) == 1)
-pres.locs2 <- coordinates(occur.ras2)[presences2, ]
-
-dens2 <- kde2d(pres.locs2[,1], pres.locs2[,2], n = c(nrow(occur.ras2), ncol(occur.ras2)))
-dens.ras2 <- raster(dens2)
-plot(dens.ras2)
-
-dens.ras2 <- extend(dens.ras2, ma.area)
-
-plot(dens.ras2)
-
-dens.work2 <- resample(dens.ras2, ma.bio)
-
-plot(dens.work2)
-
-writeRaster(dens.work2, filename = "bias_newlit.asc", format = "ascii", overwrite = TRUE)
-
->>>>>>> 140eae60836dd87ba15aedebf223b2f14a57dd47
 
